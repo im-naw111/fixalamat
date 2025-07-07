7fc8848a55437e20e3e5f6c2722113f24aff1042
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2025-07-07 08:46:41 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,27 @@
-const CACHE_NAME = "pwa-cache-v1";
+const CACHE_NAME = 'pwa-cache-v2';
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/icons/icon-192x192.png"
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/libs/swiper.min.css',
+  '/libs/swiper.min.js',
+  '/libs/fontawesome.min.css',
+  '/libs/all.min.js'
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
