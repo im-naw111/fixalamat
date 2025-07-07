4a85ed6a0f76513c5f2db04f71442bc8d6914b66
Imam Nawawi <44057098+im-naw111@users.noreply.github.com>
2025-07-07 08:47:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js').then(function () {
-      console.log('Service Worker berhasil didaftarkan!');
-    }).catch(function (error) {
-      console.log('Pendaftaran Service Worker gagal:', error);
-    });
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('Service Worker berhasil didaftarkan!'))
+      .catch(err => console.log('Gagal mendaftar SW:', err));
   });
 }
+
+const swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: true
+});
